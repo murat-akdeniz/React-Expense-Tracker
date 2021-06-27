2ee9bdd4696beb0433061f1b62ec8233bb1d41f3
Murat akdeniz <akdenizmurat023@gmail.com>
2021-06-27 19:07:12 +0300

    expense-tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import { v4 as uuidv4 } from "uuid";
+import './App.css'
+import Header from './Header'
+import getSymbolFromCurrency from 'currency-symbol-map'
+import { FaRegTrashAlt } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const arr = () => {
+  let data = localStorage.getItem('expense');
+
+  if (data) return JSON.parse(data);
+  else return [];
+
 }
 
-export default App;
+const BASE_URL = 'https://api.exchangerate.host/latest'
+
+function App() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0)
+  const [income, setIncome] = useState(0)
+  const [code, setCode] = useState();
+  const [list, setList] = useState(arr)
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setCode('$');
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);//para birimleri adları
+
+        const code = getSymbolFromCurrency(fromCurrency);
+        setCode(code);
+      })
+  }, [fromCurrency])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuidv4(),
+      name: name,
+      amount: amount
+    };
+    if (name && amount) {
+      setList([...list, newItem]);
+      setName('');
+      setAmount(0);
+
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('expense', JSON.stringify(list));
+  }, [list]);
+
+  const reducer = (accumulator, item) => {
+    return (accumulator + parseInt(item.amount));
+  }
+
+  const total = list.reduce(reducer, 0).toFixed(2);
+  const deleteItem = (id) => {
+    setList(list.filter((el) => el.id !== id));
+  }
+
+
+  return (
+    <div>
+      <Header /><br /><br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+
+        <h4>Currency:</h4>
+        <select
+          className='select'
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+        >
+          {currencyOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+          <p className="money plus">{code}{income}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+          <p className="money minus">{code}{total}</p>
+        </div>
+      </div>
+
+      <h4>Total Monthly Income:</h4>
+      <input
+        type="text"
+        placeholder="Enter Your Income"
+        value={income}
+        onChange={(e) => setIncome(e.target.value)}
+      />
+      <br />
+      <h4>Total Balance:{code}{income - total}</h4>
+      <form onSubmit={handleSubmit}>
+
+        <div className="form-control">
+          <label htmlFor="text">Item</label>
+          <input
+            type="text"
+            placeholder='Enter Item'
+            value={name}
+            onChange={(e) => {
+              if (e.target.value.length <= 15)
+                setName(e.target.value)
+            }}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="amount">Amount <br /></label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => {
+              if (e.target.value.length <= 10)
+                setAmount(e.target.value)
+            }}
+          />
+        </div>
+
+        <button className="btn">Add Expense</button>
+
+      </form>
+
+      <div className="list">
+        {list.map((item, id) => {
+          return (
+            <div key={id}>
+              <li className={item.amount > 0 ? 'plus' : 'minus'}>
+                {item.name}
+                <span>{code}{item.amount}</span>
+                <button
+                  onClick={() => deleteItem(item.id)}
+                  className='delete-btn'
+                >
+                  <FaRegTrashAlt />
+                </button>
+              </li>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
